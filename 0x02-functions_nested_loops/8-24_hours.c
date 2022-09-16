1e7ec5df7edd9ee05c61dfca325ef7c130cabb3d
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-09-16 15:58:32 -0700

    Super nested Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,16 @@
  */
 void jack_bauer(void)
 {
-	int min1, min2, h1, h2;
+	int min, hour;
 
-	for (h1 = 0; h1 < 3; h1++)
-		for (h2 = 0; h2 < 4; h2++)
-			for (min1 = 0; min1 < 6; min1++)
-				for (min2 = 0; min2 < 10; min2++)
-				{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(min1 + '0');
-					_putchar(min2 + '0');
-					_putchar('\n');
-				}
+	for (hour = 0; hour <=23; hour++)
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) = '0');
+			_putchar('\n');
+		}
 }

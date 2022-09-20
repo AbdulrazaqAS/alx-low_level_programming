b92c32e69d1467b3fc2b70ed01cfbb530c20ab27
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-09-19 21:54:34 -0700

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,13 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	
 	if (len % 2 != 0)
 		index = (len - 1) / 2;
 	else
 		index = len / 2;
 
 	for (; index < len; index++)
-		_putchar(str[index]);
+		_putchar(*(str + index));
 
 	_putchar('\n');
 }

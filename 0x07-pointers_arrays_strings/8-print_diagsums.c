af26e4accc23e1b057743f1d99c8d703dea02116
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-09-29 03:16:41 -0700

    More

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int b, c, diag_1 = 0, diag_2 = 0;
+	int b, c, diag1 = 0, diag2 = 0;
 
 	for (b = 0; b < size; b++)
 		for (c = 0; c < size; c++)
 		{
 			if (b == c)
-				diag_1 += a[b][c];
+				diag1 += a[b][c];
 			if (b + c == size -1)
-				diag_2 += a[b][c];
+				diag2 += a[b][c];
 		}
-	printf("%d, %d\n", diag_1, diag_2);
+	printf("%d, %d\n", diag1, diag2);
 }

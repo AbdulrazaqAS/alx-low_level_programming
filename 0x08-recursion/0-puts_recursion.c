97adda246fea72041f9c82b89eeddf0a26670a0f
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-09-29 17:09:14 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+		_putchar('\n');
+	_putchar(s[0]);
+	_puts_recursion(&(s + 1));
+}

a2512a2e79f3f394a2b55887aad4f506764a0dac
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-09-29 19:39:18 -0700

    Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * check_palindrome - checks
+ * @s: str
+ * @len: strlen
+ * @index: index to check
+ * Return: 1 if yes, else 0
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+	return (0);
+/**
+ * is_palindrome - checks if a str is a palindrome
+ * @s: str
+ * Return: if yes 1, else 0
+ */
+Int is_palindrome(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	if (!*s)
+		return (1);
+	return (check_palindrome(s, len, 0));
+}

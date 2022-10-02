c9e372d79159404c501908548f28387796520efd
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-02 10:06:25 -0700

    print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints num of args passed to it
+ * @argc: args count
+ * @argv: args array
+ *
+ * Return: 0 always
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

d14a33b880902c06c44dcbb43589e98a763989ef
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-02 16:29:09 -0700

    Sum args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,8 +16,10 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			if (atoi(argv[i]))
+			{
 				if (atoi(argv[i]) > 0)
 					sum += atoi(argv[i]);
+			}
 			else
 			{
 				printf("Error\n");

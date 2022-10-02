d7a4069b93abbd77d0d0aea501f72cc60008329c
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-02 15:43:35 -0700

    sum of argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: args count
+ * @argv: argv array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc > 2)
+	{
+		sum += argv[1];
+		sum += argv[2];
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}

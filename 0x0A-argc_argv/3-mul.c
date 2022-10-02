71579042807d03a9ae5bfc8fe97727109decad6c
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-02 15:54:17 -0700

    multiple of argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 2)
 	{
-		sum *= atoi(argv[1]);
-		sum *= atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 		return (0);
 	}

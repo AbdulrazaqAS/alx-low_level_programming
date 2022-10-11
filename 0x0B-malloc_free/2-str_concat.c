88483d3a0bf8075674fdc1f76928b5f45467e97e
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-11 14:36:12 -0700

    Dynamic

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,13 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, total = 0, index;
 	char *new;
 
-	if(s1 != null)
+	if(s1 != NULL)
 		while (s1[len1])
 			len1++;
-	if(s2 != null)
+	if(s2 != NULL)
 		while (s2[len2])
 			len2++;
-	total = s1 + s2;
+	total = len1 + len2;
 	new = malloc(sizeof(char) * total);
 	if (new == null)
 		return (NULL);

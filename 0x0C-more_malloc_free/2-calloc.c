91543fbbad8853090bea3754e7a316133d49b22a
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-13 03:56:54 -0700

    More

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem;
+	unsigned int index;
+	char *filler;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	mem = malloc(nmemb * size);
 	if (mem == NULL)
 		return (NULL);
-	mem = 0;
+	filler = mem;
+	for (index = 0; index < nmemb * size; index++)
+		filler[index] = '\0';
 	return (mem);
 }

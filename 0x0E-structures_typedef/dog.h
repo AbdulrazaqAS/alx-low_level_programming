01b4433a1f41b8e3b9ecafa3cb00efbff6b926b9
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-14 18:25:35 -0700

    Struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,3 @@
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
  * struct dog - dog struct
  * @name: name
@@ -12,3 +10,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);

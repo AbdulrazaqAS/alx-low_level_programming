891792baf46dfed4e329853a882921357aa0adea
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-18 15:02:02 -0700

    Fuction Pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - find index of a num in an array
+ * @array: array
+ * @size: size
+ * @cmp: compare function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL)
+		exit(0);
+	if (size <= 0)
+		return (-1);
+	for (index = 0; index < size; index++)
+		if (cmp(array[index]) > 0)
+			return (index);
+	return (-1);
+}

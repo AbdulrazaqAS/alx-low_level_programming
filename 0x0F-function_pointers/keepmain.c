1fc3f85a0a7cfa23d68033a21ee05b230c34bda6
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-21 09:37:10 -0700

    Calc

--- 0x0F-function_pointers/keepmain.c
+++ 0x0F-function_pointers/keepmain.c
@@ -16,18 +16,18 @@ int main(int argc, char **argv)
 		printf("Error");
 		exit(98);
 	}
-	if (*argv[2] != '+' || *argv[2] != '-' || *argv[2] != '*' || *argv[2] != '/' || *argv[2] != '%')
+	if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
 	{
 		printf("Error");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == '0')
 	{
 		printf("Error");
 		exit(100);
 	}
 
-	result = (get_op_func(*argv[2]))(atoi(*argv[1]), *atoi(argv[3]));
+	result = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", result);
 }

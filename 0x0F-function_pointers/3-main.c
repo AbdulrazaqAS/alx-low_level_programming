e700945120b0c2d82ff208d21038044f18b839f9
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-22 03:04:31 -0700

    Calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,27 +7,33 @@
  *
  * Return: int
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int result = 0;
+	int num1, num2, result;
+	char *op;
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' || *argv[2] != '-' || *argv[2] != '*' || *argv[2] != '/' || *argv[2] != '%')
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (op != '+' || op != '-' || op != '*' || op != '/' || op != '%' || op[1] != '\0')
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	if ((op == '/' || op == '%') && num2 == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 
-	result = (get_op_func(&(*argv[2])))(atoi(*argv[1]), atoi(*argv[3]));
+	result = get_op_func(op)(num1, num2);
 
 	printf("%d\n", result);
 }

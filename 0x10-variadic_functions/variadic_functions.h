839a0977c4a723692cbc6cf916fcc2bf4d4d0556
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-25 14:39:48 -0700

    vararg

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,8 @@
 #define VARARG
 
 #include <stdarg.h>
+#include <stddef.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif

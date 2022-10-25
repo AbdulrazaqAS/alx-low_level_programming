caba86f0c728e370d4c117f3582e024ebf4de839
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-10-25 09:08:41 -0700

    Vararg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all varargs
+ *
+ * n: fisrt arg
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int total = 0, index;
+
+	if (n == 0)
+		return (0);
+	va_start(vl, n);
+	for (index = 0; index < n; index++)
+	{
+		total += va_arg(vl, int);
+	}
+	va_end(vl);
+	return (total);
+}

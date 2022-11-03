20c865345a11e3b874b57d62232112f8e8203a80
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-03 16:54:57 -0700

    LL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,8 @@
  * add_node - adds a node to the beginning
  * @head: pointer to list
  * @str: str value of the new node
+ *
+ * Return: new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -13,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (newNode == NULL)
 		return (NULL);
-	if(str)
+	if (str)
 	{
 		strtemp = strdup(str);
 		while (*(str + len))

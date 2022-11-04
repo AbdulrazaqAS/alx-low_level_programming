115eebdaf140ddde146f9228be14cbb1bca09d29
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-04 09:29:30 -0700

    LL

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: list head
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	if (head == NULL)
+		exit(99);
+	while (head->next)
+	{
+		tmp = head;
+		head = head->next;
+		free(*tmp);
+	}
+	free(*head);
+	free(head);
+	free(tmp);
+}

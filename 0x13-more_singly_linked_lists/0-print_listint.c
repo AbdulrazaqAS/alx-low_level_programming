617dc5d0957e412795be5509b619858a66d4a6df
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-05 04:40:05 -0700

    LL

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of LL
+ * @h: head
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+	listint_t *tmp;
+
+	tmp = h;
+	while (tmp)
+	{
+		printf("%d", tmp->n);
+		tmp = tmp->next;
+		len++;
+	}
+
+	return (len);
+}

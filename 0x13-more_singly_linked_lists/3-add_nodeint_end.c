b5864be95be61b0270bf6ed0f823784fd67f7d2b
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-06 02:50:01 -0800

    LL

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 		*head = new;
 	else
-		while (*head)
+		while ((*head)->next)
 		{	
 			*head = (*head)->next;
 		}

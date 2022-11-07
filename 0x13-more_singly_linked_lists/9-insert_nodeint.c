4d62f22b459e62d548c434de1e34b4369234973d
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-07 04:32:49 -0800

    LL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,9 +27,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (*head)
+	while (*head && (*head)->next)
 	{
-		if ((*head)->next && counter == idx - 1)
+		if (counter == idx - 1)
 		{
 			new->next = (*head)->next;
 			(*head)->next = new;

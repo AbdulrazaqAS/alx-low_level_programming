611fae150f40403bcb09696ac49eefa17cc00e5c
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-07 03:17:25 -0800

    LL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = (*head)->next;
 			(*head)->next = new;
+			return (new);
 		}
 		*head = (*head)->next;
 		counter++;

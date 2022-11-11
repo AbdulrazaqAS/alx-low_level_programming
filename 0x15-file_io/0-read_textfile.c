53283e26f408773b70e92bf2b24f6c00f795af97
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-11 12:09:53 -0800

    IO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and print it
+ * @filename: filename
+ * @letters: num of letters to print
+ *
+ * Return: num of letters it could read, 0 o  failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t res;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	res = read(fd, buf, letters);
+
+	close(fd);
+
+	return (res);
+}

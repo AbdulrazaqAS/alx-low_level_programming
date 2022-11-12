1628cc9bf0c620ba23de1ea1d6cbd2da62eff07b
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-12 07:48:58 -0800

    IO

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len = 0, fd, wr;
+
+	if (!filename)
+		return (-1);
+	while (text_content[len])
+		len++;
+
+	fd = open(filename, 0_RDWR | 0_CREAT | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}

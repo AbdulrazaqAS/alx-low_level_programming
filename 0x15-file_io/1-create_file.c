0fdabb8858a69e8f7178ad5cca59c92a56c3b4cb
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-12 07:52:00 -0800

    IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	fd = open(filename, 0_RDWR | 0_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)

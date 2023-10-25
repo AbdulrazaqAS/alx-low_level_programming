a81d77ecdbde900025cb604c1d2838bca7627dcd
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2023-10-25 10:57:27 +0100

    101-wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,14 +1,4 @@
-/*
- * File: 100-wildcmp.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
+#include "main.h"
 
 /**
  * strlen_no_wilds - Returns the length of a string,

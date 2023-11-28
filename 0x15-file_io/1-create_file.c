58b53abf18f076d61e07f4fb5298067a5221f322
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2023-11-28 20:34:33 +0000

    0x15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len = 0, fd, wr;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	else
+		while (text_content[len])
+			len++;
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends to a file
+ * @filename: file name
+ * @text_content: text to append
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	else
+		while (*(text_content + len))
+			len++;
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
